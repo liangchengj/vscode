0f9bcc85d25c2bcd01c7d9c5d8d9a46851f65822
Liangcheng Juves <liangchengj@outlook.com>
2021-05-09 12:50:04 +0800

    Remove the '-' symbol between the names Use NAME variable instead of fixed name in WSL shell

--- test/unit/electron/index.js
+++ test/unit/electron/index.js
@@ -146,8 +146,8 @@ app.on('ready', () => {
 		return {
 			product: {
 				version: '1.x.y',
-				nameShort: 'Code - OSS Dev',
-				nameLong: 'Code - OSS Dev',
+				nameShort: 'Code OSS Dev',
+				nameLong: 'Code OSS Dev',
 				applicationName: 'code-oss',
 				dataFolderName: '.vscode-oss',
 				urlProtocol: 'code-oss',
